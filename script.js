7987e7d2b45587bad1006ef1a3f3187844088abf
Rajkumar99u <149999582+Rajkumar99u@users.noreply.github.com>
2026-09-17 09:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let k = ""
+for(int i=1;i<21;i++){
+	if(i%3==0)
+	{
+		k += "Fizz\n"
+	}
+	else if(i%5 == 0)
+	{
+		k += "Buzz"
+	}
+	else if(i%3 == 0 && i%5==0)
+	{
+		k += "FizzBuzz\n"
+	}
+	else{
+		k += i"\n"
+	}
+}
+alert(k)
